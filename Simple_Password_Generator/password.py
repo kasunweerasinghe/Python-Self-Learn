26f9f27dc160fb8c35595cd659ea3011b498850c
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-03-24 20:49:45 +0530

    create lettesr, numbers and symbols list

---
+++ Simple_Password_Generator/password.py
@@ -0,0 +1,14 @@
+import random
+
+# Letters
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# Numbers
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# Get USer Inputs Data
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password? ")) 
+nr_symbols = int(input(f"How many symbols would you like? "))
+nr_numbers = int(input(f"How many numbers would you like? "))
