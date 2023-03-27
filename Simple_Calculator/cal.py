f7b91a102f70b129da71543c54c59f7c1f238def
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-03-27 09:22:31 +0530

    add functions for each operations

---
+++ Simple_Calculator/cal.py
@@ -0,0 +1,17 @@
+
+# Add
+def add(n1, n2):
+    return n1 + n2
+
+# Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# divide
+def divide(n1, n2):
+    return n1 / n2
+
