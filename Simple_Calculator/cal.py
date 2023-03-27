5f0545d1ab53fec542ff7dc3c9185eb11341ab42
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-03-27 09:39:07 +0530

    fully complete

--- Simple_Calculator/cal.py
+++ Simple_Calculator/cal.py
@@ -1,3 +1,4 @@
+import os
 
 # Add
 def add(n1, n2):
@@ -25,3 +26,26 @@ operations = {
 
 # function for calculation
 def calculation():
+    num1 = float(input("Enter Your first Number: "))
+
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Enter Operator: ")
+        num2 = float(input("Enter Your second Number: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input("Do you want to continue pres 'y' start over press 'n' ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            os.system('clear')
+            calculation()
+
+
+# call the function
+calculation()
